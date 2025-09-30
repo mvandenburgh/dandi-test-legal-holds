8bee21058f2f0b56c23de899f66f6ef0ff53b347
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2025-09-30 10:59:14 -0400

    Add dandiset_bucket instance

---
+++ terraform/s3.tf
@@ -0,0 +1,25 @@
+resource "aws_iam_user" "this" {
+  name = "dandi-test-legal-holds"
+}
+
+resource "aws_iam_access_key" "this" {
+  user = aws_iam_user.this.name
+}
+
+# Based on 
+# https://github.com/dandi/dandi-infrastructure/blob/master/terraform/sponsored_bucket.tf
+module "dandi_s3_bucket" {
+  source = "git::https://github.com/dandi/dandi-infrastructure//terraform/modules/dandiset_bucket"
+
+  bucket_name                           = "dandi-test-legal-holds"
+  public                                = true
+  versioning                            = true
+  allow_cross_account_heroku_put_object = true
+  heroku_user                           = aws_iam_user.this
+  embargo_readers                       = []
+  log_bucket_name                       = "dandi-test-legal-holds-logs"
+  providers = {
+    aws         = aws
+    aws.project = aws
+  }
+}
